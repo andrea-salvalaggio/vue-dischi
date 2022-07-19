8b680d076b8943f29076488403fa08c9aab048a9
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-19 16:38:27 +0200

    stampo gli album tramite API

--- src/components/AlbumCover.vue
+++ src/components/AlbumCover.vue
@@ -1,17 +1,17 @@
 <template>
-        <div class="card">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg" class="card-img-top" alt="">
-            <div class="card-body">
-                <h3>Ten's summoner's tales</h3>
-                <p class="card-text">Sting</p>
-                <p>1993</p>
-            </div>
+    <div class="card">
+        <img :src="album.poster" class="card-img-top" :alt="album.author">
+        <div class="card-body">
+            <h3> {{ album.title }} </h3>
+            <p class="card-text"> {{ album.author }} </p>
+            <p> {{ album.year }} </p>
         </div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    props: ['album'],
 }
 </script>
 
--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -1,23 +1,50 @@
 <template>
     <div>
         <div class="row">
-            <div class="col-3">
-                <AlbumCover/>
-            </div>
+            <AlbumCover class="card-container" v-for="album in albums" :key="album.index" :album="album"/>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import AlbumCover from './AlbumCover.vue'
 
 export default {
+
+    data: function(){
+        return{
+            albums: [],
+        }
+    },
+
     components: {
-    AlbumCover,
-  }
+        AlbumCover,
+    },
+
+    methods: {
+        getAlbumCover(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((result) => {
+                console.log(result.data.response)
+                this.albums = result.data.response;
+            })
+            .catch((error) => {
+                console.warn(error);
+            })
+        }
+    },
+
+    created(){
+        this.getAlbumCover();
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    .card-container{
+        width: calc((100% / 5) - 20px);
+        margin: 10px
+    }
+    
 </style>
