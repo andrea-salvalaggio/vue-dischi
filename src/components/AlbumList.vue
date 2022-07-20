d784f9b19db34fb74187f5b5b15829ec6467381c
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-20 15:07:54 +0200

    Creo la select

--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
         <div class="row">
+            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                <option value="rock">Rock</option>
+                <option value="pop">Pop</option>
+                <option value="jazz">Jazz</option>
+                <option value="metal">Metal</option>
+            </select>
             <AlbumCover class="card-container py-3" v-for="album in albums" :key="album.index" :album="album"/>
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 import AlbumCover from './AlbumCover.vue'
 
 export default {
